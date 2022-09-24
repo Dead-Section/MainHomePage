7ad8c8d0713f4943d87487fd2d7514ef8eb24d8f
hussein bahdon <35459369+HusseinMccoin@users.noreply.github.com>
2022-09-24 12:46:39 +0100

    update code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var sheep_count, other_count;
 
 function getNumberOrString(value) {
@@ -19,12 +17,7 @@ document.getElementById('button').addEventListener('click', (event) => {
     let element_sheep_count = document.getElementById('sheep_count');
     sheep_count = (typeof sheep_count === 'number' ? sheep_count : 0) + 1;
     element_sheep_count.innerText = sheep_count;
-  }
-
-});
-
-document.getElementById('button').addEventListener('click', (event) => {
-  if (!(getNumberOrString(document.getElementById('text').value) == 'sheep')) {
+  } else {
     let element_other_count = document.getElementById('other_count');
     other_count = (typeof other_count === 'number' ? other_count : 0) + 1;
     element_other_count.innerText = other_count;
